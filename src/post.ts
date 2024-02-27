0ff9c893d1baa9452344a90836515084c47d0d5a
Shinnosuke Sawada <6warashi9@gmail.com>
2024-02-28 05:34:49 +0900

    feat: implement post step

---
+++ src/post.ts
@@ -0,0 +1,44 @@
+import * as core from "@actions/core";
+import * as artifact from "@actions/artifact";
+import { glob } from "glob";
+import Dockerode from "dockerode";
+import fs from "node:fs/promises";
+import dedent from "dedent";
+
+async function main() {
+  const jaegerDataPath = core.getInput("jaeger-data-path");
+
+  const docker = new Dockerode();
+  const container = await docker.getContainer("jaeger");
+  await container.stop();
+  await container.remove();
+
+  if (!core.getBooleanInput("upload-trace")) {
+    return;
+  }
+
+  fs.writeFile(
+    `${jaegerDataPath}/compose.yaml`,
+    dedent`
+      services:
+        jaeger:
+          image: jaegertracing/all-in-one:latest
+          ports:
+            - "16686:16686"
+          environment:
+            - "SPAN_STORAGE_TYPE=badger"
+            - "BADGER_EPHEMERAL=false"
+            - "BADGER_DIRECTORY_VALUE=/badger/data"
+            - "BADGER_DIRECTORY_KEY=/badger/key"
+          volumes:
+            - ./badger:/badger
+    `,
+  );
+
+  await artifact.default.uploadArtifact(
+    "jaeger",
+    await glob(`${jaegerDataPath}/**/*`),
+    jaegerDataPath,
+  );
+}
+main();
